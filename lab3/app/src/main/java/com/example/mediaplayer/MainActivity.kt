94f44b43b1ea26bcb920865976cdc916bcb93de9
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-12-16 02:57:15 +0300

    Moved music player functionality to MusicPlayerActivity, Main activity has two tabs for audio and video

--- lab3/app/src/main/java/com/example/mediaplayer/MainActivity.kt
+++ lab3/app/src/main/java/com/example/mediaplayer/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.mediaplayer
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var tabLayout: TabLayout
+    private lateinit var viewPager: ViewPager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        permission()
+
+        tabLayout = findViewById(R.id.tabLayout)
+        viewPager = findViewById(R.id.viewPager)
+
+        viewPager.adapter = PageAdapter(supportFragmentManager)
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+    private fun permission() {
+        if (ContextCompat.checkSelfPermission(applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE), 1);
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 1) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                permission()
+            }
+        }
     }
 }
