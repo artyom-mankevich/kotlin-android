8676877e58ac7375be0e72033f05123d6548a056
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-12-16 03:07:13 +0300

    List fragment used both for videos and music lists, PageAdapter for main tabs

---
+++ lab3/app/src/main/java/com/example/mediaplayer/PageAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.mediaplayer
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class PageAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                MusicFragment()
+            }
+            1 -> {
+                VideosFragment()
+            }
+            else -> {
+                MusicFragment()
+            }
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        when (position) {
+            0 -> {
+                return "Music"
+            }
+            1 -> {
+                return "Videos"
+            }
+        }
+        return super.getPageTitle(position)
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
