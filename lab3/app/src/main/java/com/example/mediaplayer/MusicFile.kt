717ad33322400e122d4ab82453d2e41b73ff25bf
Artyom Mankevich <artem.mankevich02@gmail.com>
2021-12-16 03:06:16 +0300

    Added MusicFile class and MusicFileAdapter for ListView

---
+++ lab3/app/src/main/java/com/example/mediaplayer/MusicFile.kt
@@ -0,0 +1,16 @@
+package com.example.mediaplayer
+
+import android.graphics.Bitmap
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+class MusicFile(
+    val path: String,
+    val title: String,
+    val artist: String,
+    val duration: String,
+    val art: Bitmap?
+) : Parcelable {
+
+}
---
+++ lab3/app/src/main/java/com/example/mediaplayer/MusicFileAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.mediaplayer
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class MusicFileAdapter(context: Context, res: Int, musicFiles: ArrayList<MusicFile>) :
+    ArrayAdapter<MusicFile>(context, res, musicFiles) {
+    private var inflater: LayoutInflater = LayoutInflater.from(context)
+    private var layout: Int = res
+    private var list: ArrayList<MusicFile> = musicFiles
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view: View = inflater.inflate(this.layout, parent, false)
+
+        val titleView: TextView = view.findViewById(R.id.titleText)
+        val artistView: TextView = view.findViewById(R.id.artistText)
+        val durationView: TextView = view.findViewById(R.id.durationText)
+        val artView: ImageView = view.findViewById(R.id.artImage)
+
+        val musicFile: MusicFile = list[position]
+
+        titleView.text = musicFile.title
+        artistView.text = musicFile.artist
+        durationView.text = musicFile.duration
+        if (musicFile.art != null) {
+            artView.setImageBitmap(musicFile.art)
+        }
+
+        return view
+    }
+}
